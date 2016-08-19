9f174108e77e781d94745f4d837fcc4e86724809
rsampdev <rsampdev@gmail.com>
2016-08-19 09:15:09 -0400

    Add the Game class and prototype some Character methods

---
+++ TheForest/Game.h
@@ -0,0 +1,13 @@
+//
+//  Game.h
+//  TheForest
+//
+//  Created by Rodney Sampson on 8/18/16.
+//  Copyright © 2016 Rodney Sampson II. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Game : NSObject
+
+@end
---
+++ TheForest/Game.m
@@ -0,0 +1,13 @@
+//
+//  Game.m
+//  TheForest
+//
+//  Created by Rodney Sampson on 8/18/16.
+//  Copyright © 2016 Rodney Sampson II. All rights reserved.
+//
+
+#import "Game.h"
+
+@implementation Game
+
+@end
